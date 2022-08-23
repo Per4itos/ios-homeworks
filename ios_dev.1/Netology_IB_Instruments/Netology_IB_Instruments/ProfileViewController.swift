1766ed46321f3b3ca92917fb05d2750641ed84e5
Adham Tangirov <tangirov97@bk.ru>
2022-08-23 13:01:25 +0300

    123

---
+++ ios_dev.1/Netology_IB_Instruments/Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by macOS on 23.08.2022.
+//
+
+import UIKit
+
+class ProfielVewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
+
+
